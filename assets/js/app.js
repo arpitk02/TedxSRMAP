f482f1046a142b0a9cfa33cb3a92064ed2ffec8f
arpitk02 <akshkumar852@gmail.com>
2020-06-21 10:44:43 +0530

    Vanta

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,7 @@ VANTA.TOPOLOGY({
     el: "#timer1",
     mouseControls: true,
     touchControls: true,
-    minHeight: 400.00,
+    minHeight: 200.00,
     minWidth: 200.00,
     scale: 1.00,
     scaleMobile: 1.00,
